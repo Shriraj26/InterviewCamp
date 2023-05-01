4d92ae72249ee5c7644c7a60de8f4dc2d1616d5f
Shriraj26 <89901228+ShrirajSDSU@users.noreply.github.com>
2023-05-01 11:07:22 -0700

    Added graph questions and some changes

--- 2. SubArray Problems/3. Prefix Sum/2. SubArrayWithSumK.py
+++ 2. SubArray Problems/3. Prefix Sum/2. SubArrayWithSumK.py
@@ -10,31 +10,30 @@ arr = [int(x) for x in input().split()]
 target = int(input('Enter the target '))
 indices = [None, None]
 
-#A dictionary to store the summ till now and index too
+# A dictionary to store the summ till now and index too
 totalDict = {}
 totalSum = 0
 
 for i in range(len(arr)):
 
-    #calculate the totalSum
+    # calculate the totalSum
     totalSum += arr[i]
 
-    #if the totalSum is 0 then return 0 to index
+    # if the totalSum is 0 then return 0 to index
     if totalSum == target:
         indices[0] = 0
         indices[1] = i
         break
 
-    #if u get the sum again this means that the sum might have been 0 in between so store the indeces here
+    # if u get the sum again this means that the sum might have been 0 in between so store the indeces here
     if totalDict.get(totalSum - target) is not None:
         indices[0] = totalDict.get(totalSum) + 1
         indices[1] = i
         break
 
-    #else store the sum in the dictionary
+    # else store the sum in the dictionary
     totalDict[totalSum] = i
 
 
 print(totalDict)
 print(indices)
-
